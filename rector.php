ed3d907941776b9cfa3e6d930bb75354af64ab23
Théo FIDRY <theo.fidry@gmail.com>
2025-02-13 23:52:05 +0100

    chore(deps): Update rector

--- rector.php
+++ rector.php
@@ -35,7 +35,6 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Php71\Rector\FuncCall\CountOnNullRector;
 use Rector\Set\ValueObject\LevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
@@ -46,9 +45,5 @@ return static function (RectorConfig $rectorConfig): void {
 
     $rectorConfig->sets([
         LevelSetList::UP_TO_PHP_81,
-    ]);
-
-    $rectorConfig->skip([
-        CountOnNullRector::class,
     ]);
 };
