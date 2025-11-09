b139ba2ca98dd766d894dc0bad001db69454f4af
Théo FIDRY <theo.fidry@gmail.com>
2025-11-09 11:16:12 +0100

    fix

--- src/SymfonyFileSystem.php
+++ src/SymfonyFileSystem.php
@@ -57,15 +57,19 @@ interface SymfonyFileSystem
      *
      * @throws FileNotFoundException When originFile doesn't exist
      * @throws IOException           When copy fails
+     *
+     * @return void
      */
-    public function copy(string $originFile, string $targetFile, bool $overwriteNewerFiles = false): void;
+    public function copy(string $originFile, string $targetFile, bool $overwriteNewerFiles = false);
 
     /**
      * Creates a directory recursively.
      *
      * @throws IOException On any directory creation failure
+     *
+     * @return void
      */
-    public function mkdir(string|iterable $dirs, int $mode = 0o777): void;
+    public function mkdir(string|iterable $dirs, int $mode = 0o777);
 
     /**
      * Checks the existence of files or directories.
@@ -80,15 +84,19 @@ interface SymfonyFileSystem
      * @param int|null                $atime The access time as a Unix timestamp, if not supplied the current system time is used
      *
      * @throws IOException When touch fails
+     *
+     * @return void
      */
-    public function touch(string|iterable $files, ?int $time = null, ?int $atime = null): void;
+    public function touch(string|iterable $files, ?int $time = null, ?int $atime = null);
 
     /**
      * Removes files or directories.
      *
      * @throws IOException When removal fails
+     *
+     * @return void
      */
-    public function remove(string|iterable $files): void;
+    public function remove(string|iterable $files);
 
     /**
      * Change mode for an array of files or directories.
@@ -98,8 +106,10 @@ interface SymfonyFileSystem
      * @param bool $recursive Whether change the mod recursively or not
      *
      * @throws IOException When the change fails
+     *
+     * @return void
      */
-    public function chmod(string|iterable $files, int $mode, int $umask = 0o000, bool $recursive = false): void;
+    public function chmod(string|iterable $files, int $mode, int $umask = 0o000, bool $recursive = false);
 
     /**
      * Change the owner of an array of files or directories.
@@ -112,8 +122,10 @@ interface SymfonyFileSystem
      * @param bool       $recursive Whether change the owner recursively or not
      *
      * @throws IOException When the change fails
+     *
+     * @return void
      */
-    public function chown(string|iterable $files, string|int $user, bool $recursive = false): void;
+    public function chown(string|iterable $files, string|int $user, bool $recursive = false);
 
     /**
      * Change the group of an array of files or directories.
@@ -127,23 +139,29 @@ interface SymfonyFileSystem
      * @param bool                    $recursive Whether change the group recursively or not
      *
      * @throws IOException When the change fails
+     *
+     * @return void
      */
-    public function chgrp(string|iterable $files, string|int $group, bool $recursive = false): void;
+    public function chgrp(string|iterable $files, string|int $group, bool $recursive = false);
 
     /**
      * Renames a file or a directory.
      *
      * @throws IOException When target file or directory already exists
      * @throws IOException When origin cannot be renamed
+     *
+     * @return void
      */
-    public function rename(string $origin, string $target, bool $overwrite = false): void;
+    public function rename(string $origin, string $target, bool $overwrite = false);
 
     /**
      * Creates a symbolic link or copy a directory.
      *
      * @throws IOException When symlink fails
+     *
+     * @return void
      */
-    public function symlink(string $originDir, string $targetDir, bool $copyOnWindows = false): void;
+    public function symlink(string $originDir, string $targetDir, bool $copyOnWindows = false);
 
     /**
      * Creates a hard link, or several hard links to a file.
@@ -152,8 +170,10 @@ interface SymfonyFileSystem
      *
      * @throws FileNotFoundException When original file is missing or not a file
      * @throws IOException           When link fails, including if link already exists
+     *
+     * @return void
      */
-    public function hardlink(string $originFile, string|iterable $targetFiles): void;
+    public function hardlink(string $originFile, string|iterable $targetFiles);
 
     /**
      * Resolves links in paths.
@@ -189,8 +209,10 @@ interface SymfonyFileSystem
      *                                   - $options['delete'] Whether to delete files that are not in the source directory (defaults to false)
      *
      * @throws IOException When a file type is unknown
+     *
+     * @return void
      */
-    public function mirror(string $originDir, string $targetDir, ?Traversable $iterator = null, array $options = []): void;
+    public function mirror(string $originDir, string $targetDir, ?Traversable $iterator = null, array $options = []);
 
     /**
      * Returns whether the file path is an absolute path.
@@ -224,13 +246,8 @@ interface SymfonyFileSystem
      * @param bool            $lock    Whether the file should be locked when writing to it
      *
      * @throws IOException If the file is not writable
-     */
-    public function appendToFile(string $filename, $content, bool $lock = false): void;
-
-    /**
-     * Returns the content of a file as a string.
      *
-     * @throws IOException If the file cannot be read
+     * @return void
      */
-    public function readFile(string $filename): string;
+    public function appendToFile(string $filename, $content, /*bool $lock = false*/);
 }
